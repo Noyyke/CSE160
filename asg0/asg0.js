eef95e859a574b2fbf03c4bc668270b19dca7fba
Nick Lafredo <nicholas@lafredo.com>
2026-03-31 19:46:06 -0700

    drawVector function

--- asg0/asg0.js
+++ asg0/asg0.js
@@ -11,6 +11,19 @@ function main() {
   var ctx = canvas.getContext('2d');
 
   // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to black
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+
+  // Using cuon-matrix-cse160.js create a function DrawVector
+  // DrawVector(v, color) that draws a vector originating at the center of the rectangle and extending to the right using cuon-matrix-cse160.js
+  function drawVector(v, color) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(v[0], v[1]);
+    ctx.lineTo(v[0], v[1]);
+    ctx.lineTo(0, 0);
+    ctx.closePath();
+  }
+  var v1 = new Vector3(2.25, 2.25, 0);
+  drawVector(v1, 'rgba(255, 0, 0, 1.0)');
 }
